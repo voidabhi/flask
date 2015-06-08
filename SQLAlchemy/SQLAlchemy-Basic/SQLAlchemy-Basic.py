9993596c01a0ed8c68f58fb4c332bb638ebf7394
Abhijeet Mohan <void.aby@gmail.com>
2015-06-08 22:39:51 +0530

    Update SQLAlchemy-Basic.py

--- SQLAlchemy/SQLAlchemy-Basic/SQLAlchemy-Basic.py
+++ SQLAlchemy/SQLAlchemy-Basic/SQLAlchemy-Basic.py
@@ -16,6 +16,7 @@ class User(db.Model):
         self.username = username
         self.email = email
 
+    # representation of object when printed
     def __repr__(self):
         return '<User %r>' % self.username
 
