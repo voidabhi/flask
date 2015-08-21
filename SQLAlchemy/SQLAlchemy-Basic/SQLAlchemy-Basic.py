b82bcf39df06771a1c4d416ae15554ee20709977
Abhijeet Mohan <void.aby@gmail.com>
2015-08-21 19:45:50 +0530

    updated comments

--- SQLAlchemy/SQLAlchemy-Basic/SQLAlchemy-Basic.py
+++ SQLAlchemy/SQLAlchemy-Basic/SQLAlchemy-Basic.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
+# initializing app and db
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
